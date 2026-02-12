23341fb2d32c5abffe96f66c1d964746d8b644ef
Scott Antwi <scottantwi930@gmail.com>
2026-02-12 12:45:51 +0000

    Add WSGI config for PythonAnywhere deployment

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+"""WSGI entry point for PythonAnywhere deployment"""
+import sys
+import os
+
+# Add project directory to path
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+from app import app as application
